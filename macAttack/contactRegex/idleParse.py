d017894eeae835e92b7df2cc60e7648cf7d0a78d
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2018-08-22 20:41:36 -0400

    init

--- macAttack/contactRegex/idleParse.py
+++ macAttack/contactRegex/idleParse.py
@@ -13,6 +13,6 @@ def checkIdleTime():
         "/usr/sbin/ioreg -c IOHIDSystem | /usr/bin/awk '/HIDIdleTime/ {print int($NF/1000000000); exit}'", shell=True)
     except subprocess.CalledProcessError:
         pass
-    return run_command
+    return int(run_command)
 
 # print checkIdleTime()
